5ea86ba63ac61720a5a70e76d496a7ad87e6588f
Enrique Lueiza <e.mimo.lueiza@gmail.com>
2023-05-27 23:37:03 -0400

    Retomamos el curso de C

---
+++ empiezaAprogramar/bloque1/bloque1.2.c
@@ -0,0 +1,12 @@
+//Hola Mundo
+
+#include <stdio.h>
+
+int main()
+{
+    printf("\nHola Mundo.\n\n\n");
+
+    getchar();
+
+    return 0;
+}
---
+++ makigas/AprendiendoC/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main ()
+{
+    printf("\Hola,Mundo.\n\n\n");
+
+    //Declaro un caracter y un numero
+
+    char x; //Es un caracter
+    int y; // Es un entero
+
+    x = 'c'; //Este es un caracter en comillas simples
+    y = 25;
+
+    int z = 2 * y;
+
+
+    printf("y = %d, x = %c, z = %d\n",y,x,z);
+
+    return 0;
+}
